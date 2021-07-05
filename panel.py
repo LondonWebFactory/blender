012e453669b58d293f1845e293794e8fccb796bc
London Web Factory <LondonWebFactory@users.noreply.github.com>
2021-07-05 14:56:14 +0100

    Create panel.py

---
+++ panel.py
@@ -0,0 +1,29 @@
+import bpy
+
+class TestPanel(bpy.types.Panel):
+    bl_label = "Test Panel"
+    bl_idname = "PT_TestPanel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = 'Adrian\'s Tools'
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        #row.label(text= "Sample Text", icon= 'CUBE')
+        #row = layout.row()
+        row.operator("mesh.primitive_cube_add", text= "Chuck a Cube On", icon= 'CUBE')
+        row = layout.row()
+        row.operator("mesh.primitive_uv_sphere_add", text= "Chuck a Sphere On", icon= 'SPHERE')
+        row = layout.row()
+        row.operator("object.text_add", text= "Chuck Some Text On", icon= 'TEXT')
+      
+        
+def register():
+    bpy.utils.register_class(TestPanel)
+    
+def unregister():
+     bpy.utils.unregister_class(TestPanel)
+     
+if __name__ == "__main__":
+    register()
